fbae4cd3b0c720637b0e26471993fb293e5bb0ef
skullknight963 <viktor.9630@hotmail.com>
2024-02-24 10:00:00 -0300

    lake: Import Graphene Camera
    * Replace Snap Camera
    
    Change-Id: Ia8ce699c5b4ecccea36724989eb93ec8ce88f6a8

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    CamX
+
 # Fingerprint Gestures
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl \
